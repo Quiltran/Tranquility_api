710c31af8ca30420754131a8447ac57e0b70e7e2
StevenAlexanderJohnson <56038965+StevenAlexanderJohnson@users.noreply.github.com>
2025-02-02 14:51:57 -0500

    adding documentation for IDatabase

--- data/database.go
+++ data/database.go
@@ -5,6 +5,7 @@ import (
 	"tranquility/models"
 )
 
+// This interface is used when creating new controllers.
 type IDatabase interface {
 	Login(ctx context.Context, cred *models.AuthUser) (*models.AuthUser, error)
 	Register(ctx context.Context, user *models.AuthUser) (*models.AuthUser, error)
