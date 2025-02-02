0e12a0dff177d91b034cf30bcfb8dacbdd8f7f4c
StevenAlexanderJohnson <56038965+StevenAlexanderJohnson@users.noreply.github.com>
2025-02-02 18:50:30 -0500

    adding request ID to request context

--- middleware/requestlog.go
+++ middleware/requestlog.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"time"
@@ -9,11 +10,18 @@ import (
 	"github.com/google/uuid"
 )
 
+type requestIDKey struct{}
+
+var RequestID requestIDKey
+
 func RequestLog(next http.Handler, logger *services.Logger) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		requestID := uuid.New().String()
 		logger.TRACE(fmt.Sprintf("Request received %s: %s %s from %s", requestID, r.Method, r.URL.Path, r.RemoteAddr))
+
+		ctx := context.WithValue(r.Context(), RequestID, requestID)
+		r = r.WithContext(ctx)
 
 		next.ServeHTTP(w, r)
 
