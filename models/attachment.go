a6cce349185c6d8c52672af598783f6df19d8d9f
StevenAlexanderJohnson <56038965+StevenAlexanderJohnson@users.noreply.github.com>
2025-02-03 22:22:27 -0500

    added db field names to attachment

--- models/attachment.go
+++ models/attachment.go
@@ -2,11 +2,12 @@ package models
 
 import "time"
 
-type AttachmentResponse struct {
-	ID          int       `json:"id,omitempty"`
-	FileName    string    `json:"file_name,omitempty"`
-	FilePath    string    `json:"file_path,omitempty"`
-	FileSize    int64     `json:"file_size,omitempty"`
-	MimeType    string    `json:"mime_type,omitempty"`
-	CreatedDate time.Time `json:"created_date,omitempty"`
+type Attachment struct {
+	ID           int       `json:"id,omitempty" db:"id"`
+	FileName     string    `json:"file_name,omitempty" db:"file_name"`
+	FilePath     string    `json:"file_path,omitempty" db:"file_path"`
+	FileSize     int64     `json:"file_size,omitempty" db:"file_size"`
+	MimeType     string    `json:"mime_type,omitempty" db:"mime_type"`
+	UserUploaded int32     `json:"user_uploaded,omitempty" db:"user_uploaded"`
+	CreatedDate  time.Time `json:"created_date,omitempty" db:"created_date"`
 }
