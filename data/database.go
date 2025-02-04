2445455a4e5a1ea6ac066c2c64d6b97eb25403a9
StevenAlexanderJohnson <56038965+StevenAlexanderJohnson@users.noreply.github.com>
2025-02-03 22:22:53 -0500

    adding CreateAttachment to IDatabase interface

--- data/database.go
+++ data/database.go
@@ -16,4 +16,5 @@ type IDatabase interface {
 	Login(ctx context.Context, cred *models.AuthUser) (*models.AuthUser, error)
 	Register(ctx context.Context, user *models.AuthUser) (*models.AuthUser, error)
 	RefreshToken(ctx context.Context, user *models.AuthUser) (*models.AuthUser, error)
+	CreateAttachment(ctx context.Context, attachment *models.Attachment) (*models.Attachment, error)
 }
